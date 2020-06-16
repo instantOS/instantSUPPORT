e0992b5e7eca352d573c8e186ebbe413a964b7ed
paperbenni <paperbenni@gmail.com>
2020-06-17 00:31:29 +0200

    fix sed

--- installserver.sh
+++ installserver.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 
-cd /usr/bin
-sudo wget serveo.surge.sh/serveo
-sudo chmod 755 serveo
+# install everything needed for the instantSUPPORT server
+
+if ! command -v serveo; then
+    cd /usr/bin
+    sudo wget serveo.surge.sh/serveo
+    sudo chmod 755 serveo
+fi
 cd
 
 if ! [ -e .ssh/id_rsa ]; then
@@ -17,7 +21,7 @@ echo "$HOME/serveo/serveo -private_key_path=$HOME/.ssh/id_rsa -port=2222" >start
 chmod 755 start.sh
 
 curl -s https://raw.githubusercontent.com/instantOS/instantSUPPORT/master/serveo.service |
-    sed "s/HOME/$HOME/g" | sudo tee /etc/systemd/system/serveo.service
+    sed "s~HOME~$HOME~g" | sudo tee /etc/systemd/system/serveo.service
 
 sudo systemctl enable serveo
 sudo systemctl start serveo
