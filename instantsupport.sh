27438b2a98fca395d97b0d5d424616857878971b
paperbenni <paperbenni@gmail.com>
2020-06-16 23:36:38 +0200

    pretty much finished

--- instantsupport.sh
+++ instantsupport.sh
@@ -1,13 +1,24 @@
 #!/bin/bash
 
+if command -v autossh && command -v tmux; then
+	echo "starting instantSUPPORT"
+else
+	echo "please install autossh and tmux"
+	exit
+fi
+
 if ! whoami | grep -q '^root$'; then
-	echo "please do not run this as root"
+	echo "please run this as root"
 	exit
 fi
 
 # todo: create support user
 # todo: tmux wrapper
 addsupport() {
+	if grep -q 'instantsupport' /etc/sudoers; then
+		echo "support user already existing"
+		return
+	fi
 	useradd -m "support" -s /usr/bin/zsh -G wheel,docker,video
 	echo "support:support" | chpasswd
 	echo "support ALL=(ALL) NOPASSWD: ALL #instantsupport" >>/etc/sudoers
@@ -21,3 +32,13 @@ if systemctl is-active sshdsystemctl is-active sshd; then
 	systemctl enable sshd
 	systemctl start sshd
 fi
+
+addsupport
+while ! [ -e /tmp/nosupport ]; do
+	autossh -M 0 -R "$2":localhost:22 support.paperbenni.xyz
+done
+
+sudo -u support tmux new -s supportsession
+sudo -u support tmux new -s supportsession
+removesupport
+pkill autossh
