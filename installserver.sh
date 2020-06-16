2aa32fa5a4a4c6f48cb5cbabf0beeced1575c2a6
paperbenni <paperbenni@gmail.com>
2020-06-17 00:26:02 +0200

    add systemd service

--- installserver.sh
+++ installserver.sh
@@ -16,6 +16,9 @@ cd serveo
 echo "$HOME/serveo/serveo -private_key_path=$HOME/.ssh/id_rsa -port=2222" >start.sh
 chmod 755 start.sh
 
+curl -s https://raw.githubusercontent.com/instantOS/instantSUPPORT/master/serveo.service |
+    sed "s/HOME/$HOME/g" | sudo tee /etc/systemd/system/serveo.service
+
 sudo systemctl enable serveo
 sudo systemctl start serveo
 
