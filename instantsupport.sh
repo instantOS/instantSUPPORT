52fe78696a2307ddce2b327f9464eadab2ed58c1
paperbenni <paperbenni@gmail.com>
2020-06-16 23:10:01 +0200

    init user

--- instantsupport.sh
+++ instantsupport.sh
@@ -7,6 +7,15 @@ fi
 
 # todo: create support user
 # todo: tmux wrapper
+addsupport() {
+	useradd -m "support" -s /usr/bin/zsh -G wheel,docker,video
+	echo "support:support" | chpasswd
+	echo "support ALL=(ALL) NOPASSWD: ALL #instantsupport" >>/etc/sudoers
+}
+
+removesupport() {
+	sed -i '/.*NOPASSWD/d' /etc/sudoers
+}
 
 if systemctl is-active sshdsystemctl is-active sshd; then
 	systemctl enable sshd
